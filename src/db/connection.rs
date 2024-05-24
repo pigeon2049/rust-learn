02ae1b8305bcfa246956aa7e256535890f0951bf
pigeon2049 <lai@live.cn>
2024-05-24 17:11:14 +0800

    use dotenv

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -3,7 +3,15 @@ use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref POOL: Pool = {
-        let url = "mysql://dev:password_2024@localhost:3306/dev";
-        Pool::new(url).expect("Failed to create connection pool")
+        // Access the environment variable at runtime
+        let database_url = std::env::var("DATABASE_URL")
+            .expect("DATABASE_URL environment variable not set");
+
+        // 将 String 转换为 &str 类型
+        let database_url_ref: &str = &database_url;
+
+        // 创建连接池
+        Pool::new(database_url_ref)
+            .expect("Failed to create connection pool")
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use crate::controller::router;
 use crate::docker::run_compose;
-
+use dotenv::dotenv;
 mod controller;
 mod docker;
 mod db;
 mod entity;
  fn main() {
+
+    dotenv().ok();
     let _runtime_docker = run_compose::main();
     let _hello_controller = router::main();
 
