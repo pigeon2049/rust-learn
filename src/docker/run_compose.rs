75540892b606286f9d62823802c182d6f74e425b
pigeon2049 <lai@live.cn>
2024-05-24 18:57:16 +0800

    init

--- src/docker/run_compose.rs
+++ src/docker/run_compose.rs
@@ -1,25 +1,11 @@
 use compose_rs::{Compose, ComposeCommand};
-const DOCKER_COMPOSE_CONTENT: &str = include_str!("../../docker-compose.yaml");
-use std::fs::File;
-use std::io::Write;
 
 #[tokio::main]
 pub(crate) async fn main() {
-
-    // Write the Docker Compose content to a temporary file
-    let temp_file_path = "temp-docker-compose.yaml";
-    let mut temp_file = File::create(temp_file_path)
-        .expect("Failed to create temporary file");
-    temp_file
-        .write_all(DOCKER_COMPOSE_CONTENT.as_bytes())
-        .expect("Failed to write to temporary file");
-
-
     let compose = Compose::builder()
-        .path(temp_file_path)
+        .path("docker-compose.yaml")
         .build()
         .unwrap();
-
 
     // Execute the `up` command to start services defined in the Docker Compose file
     if let Err(e) = compose.up().exec() {
@@ -36,8 +22,4 @@ pub(crate) async fn main() {
             println!("{:?}", service);
         });
 
-    // Remove the temporary file
-    if let Err(e) = std::fs::remove_file(temp_file_path) {
-        eprintln!("Failed to remove temporary file: {}", e);
-    }
 }
